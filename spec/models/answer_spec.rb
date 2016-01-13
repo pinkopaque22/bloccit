7ba1e2853b07af065d6130d9d78e9135b9d21791
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-13 18:45:39 +0000

    add has_many to generate belongs_to in question/answer.rb

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
+   let(:answer) { Answer.create!(body: 'Answer Body', question: question) }
+ 
+   describe "attributes" do
+     it "should respond to body" do
+       expect(answer).to respond_to(:body)
+     end
+   end
+end
