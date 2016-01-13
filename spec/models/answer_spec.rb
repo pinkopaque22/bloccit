395198eedfeb21e79717dfe34957f17919a34fea
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-13 20:09:01 +0000

    assignment-28:complete

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
-   let(:answer) { Answer.create!(body: 'Answer Body', question: question) }
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: false) }
+  let(:answer) { Answer.create!(body: 'Answer Body', question: question) }
  
    describe "attributes" do
      it "should respond to body" do
