39dc1ee006a33168e5137c2928e76645f97b6448
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-17 21:05:28 +0000

    write post_controller specs: GET new, render new_view, render new instance of @post, write post_controller specs: POST create, increment post,new by 1, assign new_post to @post, redirect to new_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,23 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
+  
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+     if @post.save
+       flash[:notice] = "Your post was saved."
+       redirect_to @post
+     else
+       flash.now[:alert] = "There was an error saving your post. Please try again."
+       render :new
+     end
+  end
+
 
   def edit
   end
