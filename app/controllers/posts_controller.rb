f31cf0a218e944379380c22038690a39fe5f3704
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-17 19:40:24 +0000

    assignment-31:complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "Spam"
+      end
+    end
   end
 
   def show
