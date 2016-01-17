fcc3ed46a35d116fba25a0d9398bc8f9303a3b1c
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-17 22:42:44 +0000

    add loop to display flash notice message with Bootstrap Markup, write specs to READ with GET show: render the show#view, assign my_post to @post, create show method to dind Posts with an :id params, view params hash,replace params hash with HTML post title and body

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -14,12 +14,20 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
- #  describe "GET show" do
- #    it "returns http success" do
- #      get :show
- #      expect(response).to have_http_status(:success)
- #    end
- #  end
+  describe "GET show" do
+    it "returns http success" do
+      get :show, {id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the #show view" do
+      get :show, {id: my_post.id}
+      expect(response).to render_template :show
+    end
+    it "assigns my_post to @post" do
+      get :show, {id: my_post.id}
+      expect(assigns(:post)).to eq(my_post)
+    end
+  end
 
   describe "GET new" do
     it "returns http success" do
