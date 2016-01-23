53ad8dff1b0849fe53976709e5213c42e56fe46e
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-23 03:29:04 +0000

    update post_controller with update method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,4 +23,17 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
+  def update
+     @post = Post.find(params[:id])
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
+ 
+     if @post.save
+       flash[:notice] = "Post was updated."
+       redirect_to @post
+     else
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :edit
+     end
+  end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -70,10 +70,27 @@ RSpec.describe PostsController, type: :controller do
     it "assigns post to be updated to @post" do
        get :edit, {id: my_post.id}
        post_instance = assigns(:post)
- 
        expect(post_instance.id).to eq my_post.id
        expect(post_instance.title).to eq my_post.title
        expect(post_instance.body).to eq my_post.body
     end
   end
+  
+  describe "PUT update" do
+     it "updates post with expected attributes" do
+       new_title = RandomData.random_sentence
+       new_body = RandomData.random_paragraph
+       put :update, id: my_post.id, post: {title: new_title, body: new_body}
+       updated_post = assigns(:post)
+       expect(updated_post.id).to eq my_post.id
+       expect(updated_post.title).to eq new_title
+       expect(updated_post.body).to eq new_body
+     end
+     it "redirects to the updated post" do
+       new_title = RandomData.random_sentence
+       new_body = RandomData.random_paragraph
+       put :update, id: my_post.id, post: {title: new_title, body: new_body}
+       expect(response).to redirect_to my_post
+     end
+   end
 end
