4748b80fe33494e84c69f5bd48b0fd6f2478b902
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-23 03:11:36 +0000

    write specs for test of edit feature, create edit methon in stub in post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,30 +2,25 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-
   def show
     @post = Post.find(params[:id])
   end
-
   def new
     @post = Post.new
   end
-  
   def create
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
-
-     if @post.save
+    if @post.save
        flash[:notice] = "Your post was saved."
        redirect_to @post
-     else
+    else
        flash.now[:alert] = "There was an error saving your post. Please try again."
        render :new
-     end
+    end
   end
-
-
   def edit
+    @post = Post.find(params[:id])
   end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -58,10 +58,22 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
- #  describe "GET edit" do
- #    it "returns http success" do
- #      get :edit
- #      expect(response).to have_http_status(:success)
- #    end
- #  end
+  describe "GET edit" do
+    it "returns http success" do
+       get :edit, {id: my_post.id}
+       expect(response).to have_http_status(:success)
+    end
+    it "renders the #edit view" do
+       get :edit, {id: my_post.id}
+       expect(response).to render_template :edit
+    end
+    it "assigns post to be updated to @post" do
+       get :edit, {id: my_post.id}
+       post_instance = assigns(:post)
+ 
+       expect(post_instance.id).to eq my_post.id
+       expect(post_instance.title).to eq my_post.title
+       expect(post_instance.body).to eq my_post.body
+    end
+  end
 end
