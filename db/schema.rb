093b87447e460e92c6ea3e8c3de569fca98cace2
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-25 05:30:14 +0000

    add foreign key to assoicate post to topic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125045500) do
+ActiveRecord::Schema.define(version: 20160125052550) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(version: 20160125045500) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
   end
+
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Comment, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
   let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
+  
     describe "attributes" do
         it "should respond to body" do
             expect(comment).to respond_to(:body)
