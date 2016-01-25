c0195de1a425860b35fa19c755f5399289f905a6
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-25 05:46:29 +0000

    fix rspec by using include RandomData in post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-
+include RandomData
 RSpec.describe Post, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
