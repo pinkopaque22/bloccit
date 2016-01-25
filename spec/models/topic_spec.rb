d109b7009cd0c5143932c95066a8245ea1eb6085
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-25 05:06:08 +0000

    add topic specs, add shoulda gem, budle install

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+    describe "attributes" do
+      it "should respond to name" do
+       expect(topic).to respond_to(:name)
+      end
+      it "should respond to description" do
+       expect(topic).to respond_to(:description)
+      end
+      it "should respond to public" do
+       expect(topic).to respond_to(:public)
+      end
+      it "should be public by default" do
+       expect(topic.public).to be(true)
+      end
+    end
 end
