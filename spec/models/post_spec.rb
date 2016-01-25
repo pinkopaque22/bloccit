ad15ba9b46ff117256ba65fdf9799a4cabca95b5
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-25 05:25:38 +0000

    add specs for post_spec, add specs for comment_spec, start association

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
  
-   describe "attributes" do
-     it "should respond to title" do
-       expect(post).to respond_to(:title)
-     end
-     it "should respond to body" do
-       expect(post).to respond_to(:body)
-     end
-   end
+  it { should belong_to(:topic) }
+    describe "attributes" do
+        it "should respond to title" do
+            expect(post).to respond_to(:title)
+        end
+        it "should respond to body" do
+            expect(post).to respond_to(:body)
+        end
+    end
   end
     
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  it { should have_many(:posts) }
     describe "attributes" do
       it "should respond to name" do
        expect(topic).to respond_to(:name)
