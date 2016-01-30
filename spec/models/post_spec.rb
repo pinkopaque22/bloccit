d944d84ad32cccb19345230a6e9995b7429bc4b9
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-30 05:04:06 +0000

    commit M from branch switching

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,16 @@ RSpec.describe Post, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
  
-  it { should belong_to(:topic) }
+  #it { should belong_to(:topic) }
+       it { is_expected.to belong_to(:topic) }
+       it { is_expected.to validate_presence_of(:title) }
+       it { is_expected.to validate_presence_of(:body) }
+       it { is_expected.to validate_presence_of(:topic) }
+     
+       it { is_expected.to validate_length_of(:title).is_at_least(5) }
+       it { is_expected.to validate_length_of(:body).is_at_least(20) }
+       
+       
     describe "attributes" do
         it "should respond to title" do
             expect(post).to respond_to(:title)
