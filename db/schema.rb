3ff228563a3156cf74140b783aa530178820fa64
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-30 18:55:02 +0000

    run migration, modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125052550) do
+ActiveRecord::Schema.define(version: 20160130184231) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -48,12 +48,31 @@ ActiveRecord::Schema.define(version: 20160125052550) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
+
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
