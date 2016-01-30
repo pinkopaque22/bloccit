7845ad99d24eab26965a67cab77044cc0dc3d52c
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-30 05:26:53 +0000

    write validation specs for :title :body :topic in  model/topic_spec write validation for :title to be >= 5 and :body >=15, :topic true in models/topic_rb

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,6 @@
 class Topic < ActiveRecord::Base
     has_many :posts, dependent: :destroy
+   validates :title, length: { minimum: 5 }, presence: true
+   validates :body, length: { minimum: 15 }, presence: true
+   validates :topic, presence: true
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2,7 +2,19 @@ require 'rails_helper'
 include RandomData
 RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  it { should have_many(:posts) }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+
+  
+      it { should have_many(:posts) }
+      it { is_expected.to belong_to(:topic) }
+      
+      it { is_expected.to validate_presence_of(:title) }
+      it { is_expected.to validate_presence_of(:body) }
+      it { is_expected.to validate_presence_of(:topic) }
+   
+      it { is_expected.to validate_length_of(:title).is_at_least(5) }
+      it { is_expected.to validate_length_of(:body).is_at_least(15) }
+     
     describe "attributes" do
       it "should respond to name" do
         expect(topic).to respond_to(:name)
@@ -13,9 +25,10 @@ RSpec.describe Topic, type: :model do
       it "should respond to public" do
         expect(topic).to respond_to(:public)
       end
+      
       #it "should be public by default" do
-          #only expect(true).to be (true) will pass this test. 
+        #only expect (true).to be (true) #will pass this test. 
         #expect(topic.public).to be(true)
       #end
     end
-end
+  end
