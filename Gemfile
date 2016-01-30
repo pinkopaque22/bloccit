f2f3956e9f78c327562e3212bf2d5f78a5826d7c
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-01-30 21:49:53 +0000

    add functionality in model/user.rb, bundle install after gem bcrypt

--- Gemfile
+++ Gemfile
@@ -30,3 +30,5 @@ source 'https://rubygems.org'
  # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
  gem 'turbolinks'
  gem 'bootstrap-sass'
+ # Used for encrypting User passwords
+ gem 'bcrypt'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+    # Shoulda tests for name
+       it { is_expected.to validate_presence_of(:name) }
+       it { is_expected.to validate_length_of(:name).is_at_least(1) }
+       
+    # Shoulda tests for email
+       it { is_expected.to validate_presence_of(:email) }
+       it { is_expected.to validate_uniqueness_of(:email) }
+       it { is_expected.to validate_length_of(:email).is_at_least(3) }
+       it { is_expected.to allow_value("user@bloccit.com").for(:email) }
+       it { should_not allow_value("userbloccit.com").for(:email) }
+       
+    # Shoulda tests for password
+       it { is_expected.to validate_presence_of(:password) }
+       it { is_expected.to have_secure_password }
+       it { is_expected.to validate_length_of(:password).is_at_least(6) }
+       
+    describe "attributes" do
+       it "should respond to name" do
+           expect(user).to respond_to(:name)
+       end
+       it "should respond to email" do
+           expect(user).to respond_to(:email)
+       end
+    end
+    
+    describe "invalid user" do
+        let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
+        let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+        let(:user_with_invalid_email_format) { User.new(name: "Bloccit User", email: "invalid_format") }
+        
+        it "should be invalid user due to blank name" do
+            expect(user_with_invalid_name).to_not be_valid
+        end
+        it "should be invalid user due to blank email" do
+            expect(user_with_invalid_email).to_not be_valid
+        end
+        it "should be invalid user due to incorrectly formatted email address" do
+            expect(user_with_invalid_email_format).to_not be_valid
+        end
+    end
 end
