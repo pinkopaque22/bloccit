2c22b9625a768b724d7f5d4a83469496b666ba2d
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-05 23:45:57 +0000

    write create method with hash params: name, email, password, password_confirmation, add flash notice on User.save to redirect to root_path or error: redirect to View#new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    def create
+        @user = User.new
+        @user.name = params[:user][:name]
+        @user.email = params[:user][:email]
+        @user.password = params[:user][:password]
+        @user.password_confirmation = params[:user][:password_confirmation]
+        
+        if @user.save
+            flash[:notice] = "Welcome to Bloccit by COF #{@user.name}!"
+            redirect_to root_path
+        else flash.now[:alert] = "Error creating account, no worries just try again, we'll wait."
+            render :new
+        end
+    end
 end
