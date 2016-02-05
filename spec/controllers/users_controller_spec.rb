8890b641456937ca4fe44742293b1be1f5bc15fa
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-05 22:03:05 +0000

    write instantiate and http specs for Controllers#new, create @var of @user to be used in Views#new, touch Views#new.html

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe UsersController, type: :controller do
         password_confirmation: "punkblocheads"
     }
     end
+    describe "GET new" do
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(:success)
+        end
+      it "instantiates a new user" do
+        get :new
+        expect(assigns(:user)).to_not be_nil
+      end
+    end
 end
