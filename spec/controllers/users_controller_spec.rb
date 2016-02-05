ce6bcf74e9e8a8c86f89f1056714ae33e49e2996
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-05 21:30:27 +0000

    create hash of new_users_attr for use throughout specs, urrrr welcome

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+    let(:new_user_attributes) do
+    {
+        name: "PunkBloccers",
+        email: "blochead@bloc.io",
+        password: "punkblocheads",
+        password_confirmation: "punkblocheads"
+    }
+    end
 end
