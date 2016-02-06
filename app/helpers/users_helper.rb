9aa3aa09348b5cc9d0507eb390053f0180f5e4a5
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-06 00:17:20 +0000

    checkpoint-37-Authentication/SignUp:complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def form_group_tag(errors, &block)
+     css_class = 'form-group'
+     css_class << ' has-error' if errors.any?
+     content_tag :div, capture(&block), class: css_class
+  end
 end
