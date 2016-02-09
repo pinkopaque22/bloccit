4a8563d0b55dc4b03b32e2172d39c73cd5952173
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-09 00:15:37 +0000

    write methods for new, create and destroy in sessions_controller, wishful code create_session, destroy_session, current_user methods in helpers/sessions_helper, include SessionsHelper in APP not controller as I will need it again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            create_session(user)
+            flash[:notice] = "Welcome to Bloccit, #{user.name}!"
+            redirect_to root_path
+        else
+            flash.now[:alert] = 'Invalid email/password combination'
+            render :new
+        end
+    end
+    def new
+    end
+    def destroy
+        destroy_session(current_user)
+            flash[:notice] = "You've been signed out, come back soon!"
+            redirect_to root_path
+    end
 end
