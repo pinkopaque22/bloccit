33cd5416c637a68a79cca78e44d8e974f127d3b1
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-10 22:15:35 +0000

    add gravatar method to application/helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
             content_tag :div, capture(&block), class: 'form-group'
         end
     end
+    def avatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    end
 end
