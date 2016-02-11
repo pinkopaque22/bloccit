c9e3ce53ac91ea5c2b8f33149803b776d4bcb6ca
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-11 22:54:56 +0000

    add test to models/user_spec to test users ability to have many posts,add user scope to models/post_spec. update models/comment_spec to associate to user and posts,update controllers/post_controller spec to associate post and user, create standalone migration for column and index

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 include RandomData
 RSpec.describe PostsController, type: :controller do
+  let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
   let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+  let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
 
   describe "GET show" do
     it "returns http success" do
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 include RandomData
 RSpec.describe Post, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
+
  
   #it { should belong_to(:topic) }
        it { is_expected.to belong_to(:topic) }
+       it { is_expected.to belong_to(:user) }
        it { is_expected.to validate_presence_of(:title) }
        it { is_expected.to validate_presence_of(:body) }
        it { is_expected.to validate_presence_of(:topic) }
+       it { is_expected.to validate_presence_of(:user) }
+
      
        it { is_expected.to validate_length_of(:title).is_at_least(5) }
        it { is_expected.to validate_length_of(:body).is_at_least(20) }
