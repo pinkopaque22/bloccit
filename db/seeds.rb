c2f9b943070e3a129e94ea64b48bcbb6cea40899
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-12 23:03:06 +0000

    seed database to create user with name, email, password; scope wishful coding for seeds with self.random_name, self.random_email; create one user to test in seed; reset the migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
 include RandomData
+ 5.times do
+   User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+ 
  15.times do
    Topic.create!(
      name:  RandomData.random_sentence,
@@ -9,6 +18,7 @@ include RandomData
  
  50.times do
    Post.create!(
+     user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -22,8 +32,13 @@ posts = Post.all
      body: RandomData.random_paragraph
    )
  end
- 
+ user = User.first
+ user.update_attributes!(
+   email: 'pinkopaque22@gmail',
+   password: 'helloworld'
+ )
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
