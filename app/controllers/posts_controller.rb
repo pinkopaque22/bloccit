a297d98f72d0d95042ab1fb0acc9017573890ff6
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-25 04:05:45 +0000

    checkpoint-40-Authorization:complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   #comment this method out when nesting to have topics redirect to show, not index
   before_action :require_sign_in, except: :show
+  before_action :authorize_user, except: [:show, :new, :create]
   def index
     @posts = Post.all
   end
@@ -55,5 +56,12 @@ class PostsController < ApplicationController
   private
    def post_params
      params.require(:post).permit(:title, :body)
+   end
+   def authorize_user
+     post = Post.find(params[:id])
+     unless current_user == post.user || current_user.admin?
+       flash[:alert] = "You must be an admin to do that."
+       redirect_to [post.topic, post]
+     end
    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -32,11 +32,18 @@ posts = Post.all
      body: RandomData.random_paragraph
    )
  end
- user = User.first
- user.update_attributes!(
-   email: 'pinkopaque22@gmail.com',
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
+
  puts "Seed finished"
  puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
