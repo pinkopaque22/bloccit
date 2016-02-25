5cfffaabf361f0d64522a4ad654d0c0729e2dece
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-25 03:21:10 +0000

    in Contoller/topics_controller add before_action filter to require sign in and authorize user for guests or redirect to index#show

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,15 +1,21 @@
 class TopicsController < ApplicationController
+ before_action :require_sign_in, except: [:index, :show]
+ before_action :authorize_user, except: [:index, :show]
     def index 
         @topics = Topic.all
     end
+    
     def show
-     @topic = Topic.find(params[:id])
+       @topic = Topic.find(params[:id])
     end
+    
     def new
-     @topic = Topic.new
-    end 
+       @topic = Topic.new
+    end
+    
     def create
-     @topic = Topic.new(topic_params)
+       @topic = Topic.new(topic_params)
+       
      if @topic.save
        redirect_to @topic, notice: "Your Topic was saved successfully."
      else
@@ -17,12 +23,15 @@ class TopicsController < ApplicationController
        render :new
      end
     end
+    
     def edit
-     @topic = Topic.find(params[:id])
+       @topic = Topic.find(params[:id])
     end
+    
     def update
-     @topic = Topic.find(params[:id])
-     @topic.assign_attributes(topic_params)
+       @topic = Topic.find(params[:id])
+       @topic.assign_attributes(topic_params)
+       
      if @topic.save
         flash[:notice] = "Topic was updated."
        redirect_to @topic
@@ -31,8 +40,9 @@ class TopicsController < ApplicationController
        render :edit
      end
     end
+    
     def destroy
-     @topic = Topic.find(params[:id])
+       @topic = Topic.find(params[:id])
  
      if @topic.destroy
        flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
@@ -42,8 +52,16 @@ class TopicsController < ApplicationController
        render :show
      end
     end
+    
     private
     def topic_params
      params.require(:topic).permit(:name, :description, :public)
+    end
+    
+    def authorize_user
+     unless current_user.admin?
+       flash[:alert] = "You must be an admin to do that."
+       redirect_to topics_path
+     end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     has_many :posts
     before_save { self.email = email.downcase }
+    before_save { self.role ||= :member }
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -11,7 +12,7 @@ class User < ActiveRecord::Base
              length: { minimum: 3, maximum: 100 },
              format: { with: EMAIL_REGEX }
 
-
    has_secure_password
+   enum role: [:member, :admin]
 end
 
