438bab3b0eb633b2dbe6307c1f451c75c062e0a9
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-25 21:51:45 +0000

    shallow nest routes for comments and posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
  resources :topics do
    resources :posts, except: [:index]
  end
-
+ 
+ resources :posts, only: [] do
+  resources :comments, only: [:create, :destroy]
+   end
+  
   get 'about' => 'welcome#about'
  resources :users, only: [:new, :create]
  resources :sessions, only: [:new, :create, :destroy]
