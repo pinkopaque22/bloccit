6f667bab17f2f6c3a4765b1c0e51f43c7276ac8f
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-25 02:34:00 +0000

    rails g AddRoleToUsers with integer as role, migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211224835) do
+ActiveRecord::Schema.define(version: 20160225023137) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20160211224835) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
