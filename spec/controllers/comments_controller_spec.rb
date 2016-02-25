4581d436debdd38eaf45c0c1b18f2d549b6c5f9e
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-25 22:35:35 +0000

    add include RandomData to controller/comments_controller to pass spec w/random data errors

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 include SessionsHelper
+include RandomData
 RSpec.describe CommentsController, type: :controller do
    let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
    let(:other_user) { User.create!(name: RandomData.random_name, email: RandomData.random_email, password: "helloworld", role: :member) }
    let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
    let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
    let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
+ 
 
-   context "guest" do
+    context "guest" do
      describe "POST create" do
        it "redirects the user to the sign in view" do
          post :create, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
          expect(response).to redirect_to(new_session_path)
        end
-   end
+     end
+ 
      describe "DELETE destroy" do
        it "redirects the user to the sign in view" do
          delete :destroy, post_id: my_post.id, id: my_comment.id
@@ -21,7 +24,7 @@ RSpec.describe CommentsController, type: :controller do
        end
      end
    end
-   
+ 
    context "member user doing CRUD on a comment they don't own" do
      before do
        create_session(other_user)
@@ -45,7 +48,7 @@ RSpec.describe CommentsController, type: :controller do
        end
      end
    end
-   
+ 
    context "member user doing CRUD on a comment they own" do
      before do
        create_session(my_user)
@@ -75,7 +78,7 @@ RSpec.describe CommentsController, type: :controller do
        end
      end
    end
-   
+ 
    context "admin user doing CRUD on a comment they don't own" do
      before do
        other_user.admin!
