55ae6e4536798e041ae32d085b2c139b621fa361
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-25 00:35:34 +0000

    assignment-39-Posts/Users:complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ActiveRecord::Base
    belongs_to :topic
    belongs_to :user
    has_many :comments, dependent: :destroy
-   default_scope { order('created_at DESC') }
+   #default_scope { order('created_at DESC') }
+   scope :ordered_by_title, -> { order('title DESC') }
+   scope :ordered_by_reverse_order, -> { order('created_at ASC') }
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: {minimum: 20}, presence: true
    validates :topic, presence: true
