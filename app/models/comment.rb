2185376bd9a8c847cd22865a393e931ee912fa4d
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-02-28 18:57:23 +0000

    create associations to topics

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  belongs_to :topics
   belongs_to :user
   
   validates :body, length: { minimum: 5 }, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
  resources :topics do
    resources :posts, except: [:index]
+   resources :comments, only: [:create, :destroy]
  end
  
  resources :posts, only: [] do
