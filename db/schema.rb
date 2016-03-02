cf678a4d7161d3544e8b9d2239c3357dcfad0d21
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-03-02 22:31:29 +0000

    add column_type to commentable_type_to_comments to fix polymorphic relationship to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229224849) do
+ActiveRecord::Schema.define(version: 20160302222213) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20160229224849) do
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "commentable_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "user_id"
+    t.string   "commentable_type"
   end
 
   add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id"
@@ -96,5 +97,16 @@ ActiveRecord::Schema.define(version: 20160229224849) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
