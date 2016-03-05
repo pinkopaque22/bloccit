04f6d276660b4f69131ae2b570dc608286eba3cb
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-03-05 01:51:47 +0000

    correct the has_many relation from commentables to comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
-    # has_many :posts
-    # has_many :comments
     has_many :posts, dependent: :destroy
-    has_many :commentable, dependent: :destroy 
+    #has_many :comments, foreign_key: :commentable_id, dependent: :destroy
+    has_many :comments, dependent: :destroy 
     has_many :votes, dependent: :destroy
     before_save { self.email = email.downcase }
     before_save { self.role ||= :member }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
        it { is_expected.to have_many(:posts) }
-       it { is_expected.to have_many(:commentables) }
+       it { is_expected.to have_many(:comments) }
        it { is_expected.to have_many(:votes) }
        it { is_expected.to validate_presence_of(:name) }
        it { is_expected.to validate_length_of(:name).is_at_least(1) }
