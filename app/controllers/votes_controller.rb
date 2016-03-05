9404ec288c4880c9d6e336a025c1df95e5774e09
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-03-05 05:11:44 +0000

    write specs for votes_controller in up-vote and down-vote, guest users redirect to sign-in before vote, signed-in user 1st vote increase by one, 2nd vote does not increase, sum increases by 1 for up-vote, decrease by 1 for down-vote, :back redirect to post or topic show,in VotesController add methods for up-vote, down-vote with :back redirects

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,3 +1,25 @@
 class VotesController < ApplicationController
-    
+    before_action :require_sign_in
+ 
+   def up_vote
+     @post = Post.find(params[:post_id])
+     @vote = @post.votes.where(user_id: current_user.id).first
+     if @vote
+       @vote.update_attribute(:value, 1)
+     else
+       @vote = current_user.votes.create(value: 1, post: @post)
+     end
+     redirect_to :back
+   end
+   
+   def down_vote
+     @post = Post.find(params[:post_id])
+     @vote = @post.votes.where(user_id: current_user.id).first
+     if @vote
+       @vote.update_attribute(:value, -1)
+     else
+       @vote = current_user.votes.create(value: -1, post: @post)
+     end
+     redirect_to :back
+   end
 end
