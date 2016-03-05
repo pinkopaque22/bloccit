c545ef0b5737aece243af3b187cd5a1b7e9f39ea
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-03-05 05:15:57 +0000

    DRY duplicate logic for up-vote/down-vote, add private method for update_vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,24 +2,22 @@ class VotesController < ApplicationController
     before_action :require_sign_in
  
    def up_vote
-     @post = Post.find(params[:post_id])
-     @vote = @post.votes.where(user_id: current_user.id).first
-     if @vote
-       @vote.update_attribute(:value, 1)
-     else
-       @vote = current_user.votes.create(value: 1, post: @post)
-     end
+     update_vote(1)
      redirect_to :back
    end
    
    def down_vote
+     update_vote(-1)
+     redirect_to :back
+   end
+   private  
+   def update_vote(new_value)
      @post = Post.find(params[:post_id])
      @vote = @post.votes.where(user_id: current_user.id).first
      if @vote
-       @vote.update_attribute(:value, -1)
+       @vote.update_attribute(:value, new_value)
      else
-       @vote = current_user.votes.create(value: -1, post: @post)
+       @vote = current_user.votes.create(value: new_value, post: @post)
      end
-     redirect_to :back
    end
 end
