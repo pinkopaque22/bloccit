959e6f666eb442be8306be593e1d89a4ca0ed881
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-03-05 04:44:09 +0000

    add POST routes at URL posts:id to paths up_vote_path, down_vote_path

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
  end
  
  resources :posts, only: [] do
-  resources :comments, only: [:create, :destroy]
-   end
+   resources :comments, only: [:create, :destroy]
+   post '/up-vote' => 'votes#up_vote', as: :up_vote
+   post '/down-vote' => 'votes#down_vote', as: :down_vote
+ end
   
   get 'about' => 'welcome#about'
  resources :users, only: [:new, :create]
