21fa73127b9733861f14a693247fb6b81aaf3855
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-03-06 22:19:30 +0000

    update migration

--- db/migrate/20160302222213_add_commentable_type_to_comments.rb
+++ db/migrate/20160302222213_add_commentable_type_to_comments.rb
@@ -1,5 +1,5 @@
 class AddCommentableTypeToComments < ActiveRecord::Migration
   def change
-    #add_column :comments, :commentable_type, :string
+    add_column :comments, :commentable_type, :string
   end
 end
