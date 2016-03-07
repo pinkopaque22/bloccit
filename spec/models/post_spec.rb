86b80d680d972881315f61c9099da4a5279c4b9a
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-03-07 23:31:24 +0000

    Assignment-43-Voting:complete

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -51,6 +51,7 @@ RSpec.describe Post, type: :model do
     end
     describe "#create_vote" do
         it "sets the post up_votes to 1" do
+            post = topic.posts.create(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
             expect(post.up_votes).to eq (1)
         end
         it "calls #create_vote when a post is created" do
