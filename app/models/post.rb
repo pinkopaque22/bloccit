0e2845c4f2454c72a2e63d668162d4cb8e11d648
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-03-07 00:43:02 +0000

    post_spec test #create_vote for current_user

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ActiveRecord::Base
    has_many :votes, dependent: :destroy
    has_many :labelings, as: :labelable
    has_many :labels, through: :labelings
+   after_create :create_vote
    
    default_scope { order('rank DESC') }
    
@@ -29,5 +30,11 @@ class Post < ActiveRecord::Base
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+   
+   private 
+   
+   def create_vote
+      user.votes.create(value: 1, post: self)
    end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -49,6 +49,19 @@ RSpec.describe Post, type: :model do
          expect( post.points ).to eq(@up_votes - @down_votes)
        end
     end
+    describe "#create_vote" do
+        it "sets the post up_votes to 1" do
+            expect(post.up_votes).to eq (1)
+        end
+        it "calls #create_vote when a post is created" do
+            post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+            expect(post).to receive(:create_vote)
+            post.save
+        end
+        it "associated the vote with the owner of the post"do 
+            expect(post.votes.first.user).to eq(post.user)
+        end
+    end
     describe "#update_rank" do
         it "calculates the correct rank" do
          post.update_rank
