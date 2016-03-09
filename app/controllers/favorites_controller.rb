ffdc992e05a4513ae8424360b0b22fe359e9e544
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-03-09 02:03:33 +0000

    write specs for creating favorites, implement the create method and code favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,13 @@
 class FavoritesController < ApplicationController
+  before_action :require_sign_in
+  def create
+    post = Post.find(params[:post_id])
+       favorite = current_user.favorites.build(post: post)
+    if favorite.save
+       flash[:notice] = "Post favorited."
+    else
+       flash[:alert] = "Favoriting failed." 
+    end
+    redirect_to [post.topic, post]
+  end
 end
