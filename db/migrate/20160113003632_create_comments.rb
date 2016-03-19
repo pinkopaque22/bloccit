659e35cc106dc386d017c3354f90b0a4f48c7225
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-03-19 19:31:38 +0000

    run rake db:test:prepare to resolve SQL exception no such column error

--- db/migrate/20160113003632_create_comments.rb
+++ db/migrate/20160113003632_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration
   def change
-    create_table :comments do |t|
+      create_table :comments do |t|
       t.text :body
       t.references :post, index: true, foreign_key: true
 
