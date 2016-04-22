6f7407c9b8d30462eeeb1e5dcbfd05843c783b42
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-04-22 22:38:04 +0000

    Assignment-45-Public-Profiles:complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+   
     def new
         @user = User.new
     end
@@ -19,6 +20,12 @@ class UsersController < ApplicationController
     end
     def show
      @user = User.find(params[:id])
+    end
+    def avatar_url(size)
+     gravatar_id = Digest::MD5::hexdigest(self.name).downcase
+     "https://s-media-cache-ak0.pinimg.com/favicons/fe2172ad86a337a478ad3ad852faf809887f550cb63f7ff1bb2de5f9.png?
+      4a1dbe3038214acc47b3bd3dddec2425
+      /#{gravatar_id}.png?s=#{size}"
     end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,10 @@ class User < ActiveRecord::Base
    def favorite_for(post)
      favorites.where(post_id: post.id).first
    end
+   def avatar_url(size)
+     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+   end
+
 end
 
