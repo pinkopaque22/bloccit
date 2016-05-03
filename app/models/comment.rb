f5e098da109b18813d6d4f55a23cda01ccaa5c28
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-05-03 21:46:07 +0000

    refactor rspec via manual cherry pick to pass tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
+  
+  belongs_to :commentable, polymorphic: true
   belongs_to :user
-  belongs_to :comments, :polymorphic => true
-
   
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
