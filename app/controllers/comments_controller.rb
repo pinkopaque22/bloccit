6863c198f37da87d6ce09d62cb610f0f4c0cbf5d
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-05-03 21:46:07 +0000

    refactor rspec via manual cherry pick to pass tests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -34,10 +34,17 @@ class CommentsController < ApplicationController
      params.require(:comment).permit(:body)
    end
    def authorize_user
-     comment = Comment.find(params[:id])
-     unless current_user == comment.user || current_user.admin?
+     @comment = Comment.find(params[:id])
+     unless current_user == @comment.user || current_user.admin?
        flash[:alert] = "You do not have permission to delete a comment."
-       redirect_to [comment.post.topic, comment.post]
+       if params[:post_id]
+         @commentable = Post.find(params[:post_id])
+           redirect_to [@commentable.topic, @commentable]
+       else
+         @commentable = Topic.find(params[:topic_id])
+           redirect_to @commentable
+       end
      end
    end
+
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe CommentsController, type: :controller do
    let(:other_user) { User.create!(name: RandomData.random_name, email: RandomData.random_email, password: "helloworld", role: :member) }
    let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
    let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
-   let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
+   let(:my_comment) { Comment.create!(body: 'Comment Body', commentable_id: my_post.id, commentable_type: "Post", user: my_user) }
+
  
 
     context "guest" do
