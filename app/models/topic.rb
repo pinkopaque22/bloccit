f6b5e2eada15adf807c1e9826f5bf7f3b24a6b1c
Patricia Ehrhardt <pinkopaque22@gmail.com>
2016-05-08 21:03:26 +0000

    GUI working for app for private topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,14 +1,11 @@
 class Topic < ActiveRecord::Base
     has_many :posts, dependent: :destroy
-<<<<<<< HEAD
-=======
-    #has_many :commentable, foreign_key: :commentable_id, dependent: :destroy
-    has_many :comments
->>>>>>> checkpoint-48-API-retrieving-data
     has_many :labelings, as: :labelable
     has_many :labels, through: :labelings
     #has_many :commentable, foreign_key: :commentable_id, dependent: :destroy
-    has_many :comments
+    #has_many :comments
+    has_many :comments, foreign_key: :commentable_id, dependent: :destroy
+
     
    scope :visible_to, -> (user) { user ? all : where(public: true) }
 
